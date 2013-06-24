801f453ed323e8bf37eeb10191c922e8d6efb6fd
client-c1 <client-c1@client-c1.(none)>
2013-06-24 13:30:53 +0700

    added test3.java

--- src/com/justudin/test3a.java
+++ src/com/justudin/test3a.java
@@ -3,6 +3,10 @@ package com.justudin;
 public class test3a {
 	public static void main(String[] args){
 		//System.out.println("Hello World");
-		
+		for(int a=1; a<7; a++){
+			for(int b=1; b<=a; b++)
+				System.out.print(b);
+				System.out.println("");
+		}
 	}
 }
---
+++ src/com/justudin/test4.java
@@ -0,0 +1,5 @@
+package com.justudin;
+
+public class test4 {
+
+}
