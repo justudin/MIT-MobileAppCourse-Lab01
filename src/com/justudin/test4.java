e5eabe887150f72c007fdaf0a1add55e71b73d5b
Muhammad Syafrudin <just.udin@yahoo.com>
2013-06-24 15:00:45 +0700

    added test5.java

--- src/com/justudin/test4.java
+++ src/com/justudin/test4.java
@@ -2,12 +2,21 @@ package com.justudin;
 
 public class test4 {
 	public static void main(String[] args){
-		
+		int[] aArr = {1,2,3,4,5};
+		int[] rArr = reduce(aArr);
+		for(int b=1;b<rArr.length;b++)
+		{
+			System.out.print(rArr[b]+" ");
+		}	
 	}
 	
 	public static int[] reduce(int[] a)
 	{
-		
-		return a;
+		int[] rArr = new int[5];
+		for(int b=1;b<a.length;b++)
+		{
+			rArr[b]=a[b]+a[b-1];
+		}
+		return rArr;
 	}
 }
